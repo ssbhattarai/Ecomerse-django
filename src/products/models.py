5df91e75086e47d29568c3374bfa8fd692f6b79d
ssbzeref <nascentsurya@gmail.com>
2019-06-10 13:40:53 +0545

    image field added

--- src/products/models.py
+++ src/products/models.py
@@ -15,14 +15,17 @@ def upload_image_path(instance, filename):
     print(instance)
     print(filename)
     new_filename = random.randint(1, 3214562)
-    return new_filename
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(
+        new_filename=new_filename, ext=ext)
+    return "products/{new_filename}/{final_filename}".format(new_filename=new_filename, final_filename=final_filename)
 
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
-    image = models.FileField(upload_to="products/", null=True, blank=True)
+    image = models.ImageField(upload_to="products/", null=True, blank=True)
 
     def __str__(self):
         return self.title
